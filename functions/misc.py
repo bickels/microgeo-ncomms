5db6529a50361c9c1f39fc085ef87304d26885ba
Samuel Bickel <samuel.bickel@usys.ethz.ch>
2019-01-18 13:46:19 +0100

    reduce content

--- functions/misc.py
+++ functions/misc.py
@@ -5,100 +5,45 @@ Created on Thu Jun 28 18:47:57 2018
 @author: bickels
 """
 import numpy as np
-import scipy.ndimage as ndi
 
 #%%
-def rle(a):
-    """ run length encoding. Partial credit to R rle function.
-        Multi datatype arrays catered for including non Numpy
-        returns: tuple (runlengths, startpositions, values) """
-    ia = np.array(a)                  # force numpy
-    n = len(ia)
-    if n == 0:
-        return (None, None, None)
-    else:
-        y = np.array(ia[1:] != ia[:-1])     # pairwise unequal (string safe)
-        i = np.append(np.where(y), n - 1)   # must include last element posi
-        z = np.diff(np.append(-1, i))       # run lengths
-        p = np.cumsum(np.append(0, z))[:-1] # positions
-        return (z, p, ia[i])
-
-def lin_gapfill(x):
-    try:
-        ind = np.arange(len(x))
-        not_nan = ~np.isnan(x)
-        return np.interp(ind,ind[not_nan], x[not_nan])
-    except:
-        return x
-
-def sum_chunk(x, chunk_size, axis=-1):
-    shape = x.shape
-    if axis < 0:
-        axis += x.ndim
-    shape = shape[:axis] + (-1, chunk_size) + shape[axis+1:]
-    x = x.reshape(shape)
-    return x.sum(axis=axis+1)
-
-def moving_mean(x,y,n, dropna=False):
-    xy = np.array([x,y])
-    if dropna:
-        xy = xy[:,xy[0].argsort()]
-        bna = np.isnan(xy).any(axis=0)
-        xy = xy[:,~bna]
-    else:
-        xy = xy[:,xy[0].argsort()]
-    return xy[0], np.convolve(xy[1],np.ones(n)/n,mode='same')
-
 def rank(v):
+    """ returns ranks of values
+
+    Parameters
+    ----------
+    v : list or ndarray
+        values to be ranked
+
+    Returns
+    -------
+    ndarray
+        ranks of values (descending)
+    """
     ranks = np.empty(len(v),dtype='int')
     ranks[np.argsort(-v)] = np.arange(1,len(v)+1)
     return ranks
 
-def radial_mask(a, b, nx, ny, r=1):
-    xm, ym = np.ogrid[-a:nx-a, -b:ny-b]
-    return xm*xm + ym*ym <= r*r
-
-def radial_mean(a, pos=(0,0),bins=20):
-    sx, sy = a.shape
-    ox,oy = pos
-    X,Y = np.ogrid[0:sx,0:sy]
-    r = np.hypot(X - ox, Y - oy)
-    rbin = (bins* r/r.max()).astype(np.int)
-    return ndi.mean(a, labels=rbin, index=np.arange(1, rbin.max() +1))
-
-def rms(sim,obs):
-    return np.mean((sim-obs)**2)**0.5
-
-#%% FILE HANDLING
-def query_spatial(data, desc, loc, out='colrow', dataout=None):
-    loc = np.asarray(loc)
-    height = desc['height']
-    width = desc['width']
-    af = desc['transform']
-    locout = np.zeros_like(loc)
-    if out == 'colrow':
-        for i, l in enumerate(loc):
-            locout[i] = ~af*l
-    elif out == 'xy':
-        for i, l in enumerate(loc):
-            locout[i] = af*l
-    col,row = locout.T
-    bNA = np.isnan(col) | np.isnan(row) | (row>height) | (col>width)
-    if dataout == None:
-        dataout = np.zeros_like(bNA,dtype=float)
-    dataout[~bNA] = data[row[~bNA].astype(int),col[~bNA].astype(int)]
-    return dataout, bNA
-
-#def read_soilgrids(f,loc):
-#    with rio.open(f) as X:
-#        rows,cols = np.array([X.index(lo-360,lt) for lo,lt in loc]).T
-#        x = X.read(1)[rows,cols]
-#    x = x.astype('float')
-#    x[x==X.meta['nodata']] = np.nan
-#    return x
-
 #%% CONSTANTS
 def constants(key,val=None,mode='r'):
+	""" helper function to import/export constants to textfile as python dictionaries
+
+    Parameters
+    ----------
+	key : str
+        key for value
+
+	val : value
+        value to retrieve. None (default) or value to save if mode 'a'.
+	
+    mode: str
+        either 'r'(default) for read or 'a' for append
+
+    Returns
+    -------
+    value
+        value in dictionary
+    """
     with open('CONSTANTS.txt', mode) as f:
         if val:
             f.write('\ndict('+key+'='+str(val)+')')
--- functions/model.py
+++ functions/model.py
@@ -8,60 +8,6 @@ import numpy as np
 import scipy.constants as const
 
 #%% SOIL
-def surfaceA_tex(tex, rhosoil, por=False, rhopart=2.65e6, dataout=None):
-    fsand,fsilt,fclay = tex
-    #rhosoil = 1.35e6 #g m**-3
-    rsand = 1e-3 #m
-    rsilt = 2.5e-5 #m
-    rclay = 1e-6 #m
-
-    pVsand = 4/3.*np.pi*rsand**3
-    pVsilt = 4/3.*np.pi*rsilt**3
-    pVclay = rclay**2*np.pi*2e-8
-    pVclay2 = rclay**2*np.pi*1e-7
-
-    #Vsolid = rhosoil/rhopart
-    if por.any():
-        Porosity = por
-    else:
-        Porosity = 1.-(rhosoil/rhopart)
-
-    Vsolid = rhosoil**-1.*(1.-Porosity)
-
-    Vsand = fsand*Vsolid
-    Vsilt = fsilt*Vsolid
-    Vclay = fclay*Vsolid
-
-    Gsand = Vsand/pVsand
-    Gsilt = Vsilt/pVsilt
-    Gclay = Vclay/(pVclay+pVclay2)
-
-    pAsand = 4*np.pi*rsand**2
-    pAsilt = 4*np.pi*rsilt**2
-    pAclay = 2*np.pi*rclay**2+2*np.pi*rclay*2e-8
-    pAclay2 = 2*np.pi*rclay**2+2*np.pi*rclay*1e-7
-
-    Asand = Gsand*pAsand
-    Asilt = Gsilt*pAsilt
-    Aclay = Gclay*np.mean([pAclay,pAclay2])
-
-    if dataout == None:
-        return Asand+Asilt+Aclay
-    else:
-        dataout = Asand+Asilt+Aclay
-        return dataout
-
-def inv_vG(wc, theta_r, theta_s, alpha, n):
-    sat = (wc-theta_r)/(theta_s-theta_r)
-    m = 1.-(1./n)
-    return (1./alpha)*(sat**(-1./m)-1.)**(1./n)
-
-def MQ_Deff(Ds, wc, wc_s):
-    """ Millington, Quirk
-        Ds, wc, wc_s
-    """
-    return Ds*wc**(10.0/3.0)/wc_s**2
-
 def climatic_watercontent(tau, pet, awc, d=1):
     awcFC = 0.5*awc
     alpha = pet/(awcFC*d)
@@ -83,8 +29,7 @@ def theta_ptf(OC,Cl,Si,BD,depth):
     '''Toth et al 2015 https://onlinelibrary.wiley.com/doi/pdf/10.1111/ejss.12192'''
     T = depth <= 0.3
     theta_S = 0.6819-0.06480*(1./(OC+1))-0.11900*BD**2-0.02668*T+0.001489*Cl+0.0008031*Si+0.02321*(1./(OC+1))*BD**2+0.01908*BD**2*T-0.0011090*Cl*T-0.00002315*Si*Cl-0.0001197*Si*BD**2-0.0001068*Cl*BD**2
-    theta_FC = 0.2449-0.1887*(1./(OC+1))+0.004527*Cl+0.001535*Si+0.001442*Si*(1./(OC+1))-0.00005110*Si*Cl+0.0008676*Cl*(1./(OC+1))
-    return theta_S,theta_FC
+    return theta_S
 
 #%% NPP
 def miamiNPP(MAT,MAP,a,b,c,d):
@@ -94,18 +39,11 @@ def miamiNPP(MAT,MAP,a,b,c,d):
 #    c = 6.34
 #    d = 7.5
 #    e = -0.13
-#    nppT = 3000*(1.+np.exp(1.315-0.119*MAT))**-1.
-#    nppP = 3000*(1.-np.exp(-0.000664*MAP))
     nppT = a*(1.+np.exp(b-c*MAT))**-1
     nppP = a*(1.-np.exp(d*MAP))
     return np.min(np.stack([nppT,nppP]),axis=0)
 
 #%% MICROBIAL
-def muTe(T,A,dG,alpha,B,Tmax):
-    '''http://onlinelibrary.wiley.com/doi/10.1111/j.1750-3841.2011.02377.x/full
-    eq.7'''
-    return A*T*np.exp(-(dG/(const.R*T))**alpha)*(1-np.exp(B*(T-Tmax)))
-
 def muTs(T, T0, TL, TH, dH0, dHL, dHH):
     '''schoolfield'''
     T,T0,TH,TL = const.convert_temperature([T,T0,TH,TL],'C','K')
@@ -114,63 +52,18 @@ def muTs(T, T0, TL, TH, dH0, dHL, dHH):
 def satf(x,v,K):
     return v*x/(K+x)
 
-def Mmic(x,D,Ms,m,Y):
-    return -(D*Ms*Y)/(D+m*x)
-
-def de(D,Fs,m,Mc,Y,Ac):
-    Fb = (m*Mc)/(Y*Ac)
-    return -(D/m)**0.5*np.log(Fb/Fs)
-
-def Ccss(x,D,Fs,m):
-    return (Fs/(2*(D*m)**0.5))*np.exp(-x*(m/D)**0.5)
-
-def dCcss(x,D,Fs,m):
-    return -(np.exp(-(m/D)**0.5*x)*Fs)/(2*D)
-
-def SAR(A,c,z):
-    return c*A**z
-
-def GNBD(mu,a,b):
-    #'''cang hui et al 2009'''
-    #return 1.-(a*mu**(b-1.))**(mu/(1.-a*mu**(b-1)))
-    '''gaston & he 2003'''
-    var = a*mu**b
-    return 1.-(mu/var)**(mu**2/(var-mu))
-
 def cells_from_npp(npp, mat, m, fC, muTpars=(25,9.85,41.5,-5.43e3,-141.1e3,687.9e3), d=1.):
     mt = m*muTs(mat,*muTpars)
     Ncells = npp/(mt*fC*d)
     return Ncells
 
 #%% percolation
-import mpmath as mp
-mp_log = np.frompyfunc(mp.log, 1, 1)
-mp_exp = np.frompyfunc(mp.exp, 1, 1)
-mp_pow = np.frompyfunc(mp.power, 2, 1)
-
 def ns(s,se,tau, log=False):
     if log:
         return -tau*np.log(s)+s/se
     else:
 #        return mp_pow(s,-tau)*mp_exp(-s/se)
         return s**-tau*np.exp(-s/se)
-
-def ns_approx(s,p,pc,tau,sigma,q,K,b):
-    '''hunt 2014; Stauffer 1979'''
-    return K*s**-tau*np.exp(-b*(s**sigma*abs(p-pc))**q)
-
-def ns_super(s,p,pc,theta,d,K,b):
-    '''http://iopscience.iop.org/article/10.1088/0305-4470/14/5/011/pdf'''
-    return K*s**(theta)*np.exp(-b*(p-pc)*s**(1.-1./d))
-#    epsilon = np.ones_like(p)
-#    bapc = p>pc
-#    epsilon[bapc] = (1.-1./d)
-#    return K*s**(theta)*np.exp(-b*(p-pc)*s**epsilon)
-    
-def ns_super2(s,se,tau,d,K,A):
-    '''https://journals.aps.org/pre/pdf/10.1103/PhysRevE.66.016124'''
-    dd=(d-1.)/d
-    return K*s**-tau*np.exp(-s**dd/se-A*dd*s**dd/se)
 
 def Pp_logistic(p, pc, k, e=1., sm=1.):
 #    return p/mp_pow(1.+mp_exp(-sm*k*(p-pc)),e)
@@ -186,12 +79,16 @@ def Pplo(p, pc, L, ks=[0.39325394,  0.01192326],sm=1, e=1):#, es=[1.48357842, 0.
 #    e = scalepar(L,*es)
     return Pp_logistic(p, pc, k, sm=sm,e=e)
 
-def aqueous_clusters(p, por, L, d, dx, s, sm=1.):
+def aqueous_clusters(p, por, L, d, dx, s, sm=1., pc=False):
     L0 = int(L/dx)
     N0 = int(L**d/dx**d)
     
     if d==2:
-        pc = 0.592746*por
+        if pc:
+            pc *= por
+        else:
+            # site percolation square grid
+            pc = 0.592746*por
         v = 4/3.
         beta = 5/36.
         sigma = 36/91.
@@ -200,7 +97,11 @@ def aqueous_clusters(p, por, L, d, dx, s, sm=1.):
         tau = (d/df)+1.
     
     if d==3:
-        pc = 0.31160*por
+        if pc:
+            pc *= por
+        else:
+            # site percolation simple cubic
+            pc = 0.31160*por
         v = 0.87
         beta = 0.4181
         sigma = 0.445
